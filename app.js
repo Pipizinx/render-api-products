12e17612556a6a2f5baab92f1dd626f27949a07d
I2HN <pietro.schiavinato@aluno.senai.br>
2026-06-15 08:33:14 -0300

    update

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(errorMiddleware);
 async function inicializarBanco() {
   const client = new Client({
     connectionString:
-      "postgres://admin:vLdQ7edWKnLhO0s5IOJpYsa61UVOhQtt@dpg-d8nksburnols73dsn8ig-a.ohio-postgres.render.com/products_db_vaav?ssl=true",
+      "postgresql://admin:NP5VLEu2i0bdrdURYo0NKCUfCw4X9fOn@dpg-d8ntotr7uimc73a8a3ng-a/db_products_nzmz?ssl=true"
+      // "postgres://admin:vLdQ7edWKnLhO0s5IOJpYsa61UVOhQtt@dpg-d8nksburnols73dsn8ig-a.ohio-postgres.render.com/products_db_vaav?ssl=true",
   });
 
   try {
